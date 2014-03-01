c64f5bb7fa1c976835d6e63e2022f0a4cd8022bd
Naga Raju Bhanoori <bhanoori@usc.edu>
2014-02-28 16:28:10 -0800

    sdlfjsdf

--- compute.sh
+++ compute.sh
@@ -1,13 +1,13 @@
-apt-get install nova-novncproxy novnc nova-api \
+#apt-get install nova-novncproxy novnc nova-api \
   nova-ajax-console-proxy nova-cert nova-conductor \
   nova-consoleauth nova-doc nova-scheduler \
   python-novaclient
-keystone user-create --name=nova --pass=token --email=bhanoori@usc.edu
-keystone user-role-add --user=nova --tenant=service --role=admin
-#keystone service-create --name=nova --type=compute \
+#keystone user-create --name=nova --pass=token --email=bhanoori@usc.edu
+#keystone user-role-add --user=nova --tenant=service --role=admin
+keystone service-create --name=nova --type=compute \
   --description="Nova Compute service"
-#keystone endpoint-create \
-#  --service-id=$(keystone service-list | awk '/ Compute / {print $2}') \
-#  --publicurl=http://controller:5000/v2.0 \
-#  --internalurl=http://controller:5000/v2.0 \
-#  --adminurl=http://controller:35357/v2.0
+keystone endpoint-create \
+  --service-id=$(keystone service-list | awk '/ compute / {print $2}') \
+  --publicurl=http://controller:5000/v2.0 \
+  --internalurl=http://controller:5000/v2.0 \
+  --adminurl=http://controller:35357/v2.0
