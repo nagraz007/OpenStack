79d4894d7228167539080b35d01365af5927abf8
Naga Raju Bhanoori <bhanoori@usc.edu>
2014-03-01 22:18:41 -0800

    ldsjfskdfhjksdjf

--- NLP/pospreproc.py
+++ NLP/pospreproc.py
@@ -11,11 +11,11 @@ print "Mode : ",mode
 
 if mode == "train":
     infile = open("train.pos","r")
-    outfile = open("tf1prevpos_train.tx","w")
+    outfile = open("tf3prevpos_train.tx","w")
 elif mode == "dev":
     infile = open("dev.pos","r")
-    outfile = open("tf1prevpos_dev.tx","w")
-    tagfile = open("tf1prevpos_devtags.tx","w")
+    outfile = open("tf3prevpos_dev.tx","w")
+    tagfile = open("tf3prevpos_devtags.tx","w")
 elif mode == "test":
     print "Test Mode"
 
@@ -44,28 +44,34 @@ if mode == "train"  or mode == "dev":
 #                 pairs[i+1][1] = pairs[i+1][1].replace('#', '0')
             
             if mode =="train":
-                outfile.write(pairs[i][1]+" " +"w:"+pairs[i][0]+ " ws:"+pairs[i][0][-3:]+" ")
+                outfile.write(pairs[i][1]+" " +"w:"+pairs[i][0]+ " ws:"+pairs[i][0][-3:]+" wbs:"+pairs[i][0][-2:]+" ")
                 #outfile.write(pairs[i][1]+" "+pairs[i][0]+"\n")
             else:
-                outfile.write("w:"+pairs[i][0]+ " ws:"+pairs[i][0][-3:]+" ")
+                outfile.write("w:"+pairs[i][0]+ " ws:"+pairs[i][0][-3:]+" wbs:"+pairs[i][0][-2:]+" ")
                 tagfile.write(pairs[i][1]+"\n")
                 #outfile.write("? "+pairs[i][0]+"\n")
                 #outfile.write(pairs[i][1]+" "+pairs[i][0]+"\n")
                 #tagfile.write(pairs[i][1]+"\n")
                 
             if i==0:
-                outfile.write("pw:0 pt:0 pt2:0 ")
+                #outfile.write("pw:0 ps:0 p2w:0 ")
+                outfile.write("pw:0 pt:0 pt2:0 ps:0 pbs:0 ")
             elif i == 1:
-                outfile.write("pw:"+pairs[0][0]+" pt:" + pairs[0][1]+ " pt2:0 ")
+                #outfile.write("pw:"+pairs[0][0]+" ps:"+pairs[0][0][-3:]+ " p2w:0" +" ")
+                outfile.write("pw:"+pairs[0][0]+" pt:" + pairs[0][1]+ " pt2:0" + " ps:"+pairs[0][0][-3:]+ " pbs:"+pairs[0][0][-2:]+ " ")
             else:
-                outfile.write("pw:"+pairs[i-1][0]+" pt:" + pairs[i-1][1] +" "+ "pt2:"+pairs[i-2][1]+ " ")
+                #outfile.write("pw:"+pairs[i-1][0]+ " ps:"+pairs[i-1][0][-3:]+ " p2w:"+pairs[i-2][0]+ " ")
+                outfile.write("pw:"+pairs[i-1][0]+" pt:" + pairs[i-1][1] + " pt2:"+pairs[i-2][1]+ " ps:"+pairs[i-1][0][-3:]+ " pbs:"+pairs[i-1][0][-2:]+" ")
                 #outfile.write(pairs[i-1][0] + " ")
              
-			if i+1 < len(pairs):
-				outfile.write("nw:"+pairs[i+1][0] + " ns:"+pairs[i+1][0][-3:]+"\n")
-			else:
-				outfile.write("nw:0 ns:0\n")
-			#outfile.write(pairs[i][1]+" "+pairs[i][0]+"\n")
+            if i+2 < len(pairs):
+                outfile.write("nw:"+pairs[i+1][0] + " ns:"+pairs[i+1][0][-3:]+ " nbs:"+pairs[i+1][0][-2:]+ " n2s:"+pairs[i+2][0][-3:]+"\n")
+            elif i+1 < len(pairs):
+                outfile.write("nw:"+pairs[i+1][0] + " ns:"+pairs[i+1][0][-3:]+ " nbs:"+pairs[i+1][0][-2:]+ " n2s:0"+"\n")
+            else:
+                outfile.write("nw:0 ns:0 nbs:0 n2s:0\n")
+                #outfile.write(pairs[i][1]+" "+pairs[i][0]+"\n")
+            
             
 infile.close()            
 outfile.close()
