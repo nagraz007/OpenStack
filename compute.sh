1bc7fbe07840f00871c738df21692d47d6ca916e
Naga Raju Bhanoori <bhanoori@usc.edu>
2014-03-03 15:12:09 -0800

    chnged many

--- compute.sh
+++ compute.sh
@@ -1,13 +1,17 @@
 #apt-get install nova-novncproxy novnc nova-api \
-  nova-ajax-console-proxy nova-cert nova-conductor \
-  nova-consoleauth nova-doc nova-scheduler \
-  python-novaclient
+ # nova-ajax-console-proxy nova-cert nova-conductor \
+ # nova-consoleauth nova-doc nova-scheduler \
+  #python-novaclient
 #keystone user-create --name=nova --pass=token --email=bhanoori@usc.edu
 #keystone user-role-add --user=nova --tenant=service --role=admin
-keystone service-create --name=nova --type=compute \
-  --description="Nova Compute service"
+#keystone service-create --name=nova --type=compute \
+ # --description="Nova Compute service"
+adminid=$(keystone tenant-list | awk '/ admin / {print $2}')
+serviceid=$(keystone tenant-list | awk '/ admin / {print $2}')
+naga="asas"
+public="http://controller:8774/v2/%\$adminid\$serviceid\"
 keystone endpoint-create \
   --service-id=$(keystone service-list | awk '/ compute / {print $2}') \
-  --publicurl=http://controller:5000/v2.0 \
-  --internalurl=http://controller:5000/v2.0 \
-  --adminurl=http://controller:35357/v2.0
+  --publicurl=$public \
+  --internalurl=$public \
+  --adminurl=$public
